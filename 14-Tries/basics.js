d8f1463c8cf9860b0ebc151e854e0ce6c2901598
Aljo00 <aljojoju950@gmail.com>
2025-03-22 17:04:09 +0530

    Added trie algorithms

---
+++ 14-Tries/basics.js
@@ -0,0 +1,62 @@
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEnd = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let node = this.root;
+
+    for (let char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+
+      node = node.children[char];
+    }
+
+    node.isEnd = true;
+  }
+
+  findOne(prefix) {
+    let node = this.root;
+
+    for (let char of prefix) {
+      if (!node.children[char]) return null;
+
+      node = node.children[char];
+    }
+
+    return node;
+  }
+
+  collectWords(node, prefix, result) {
+    if (node.isEnd) result.push(prefix);
+
+    for (let char in node.children) {
+      this.collectWords(node.children[char], prefix + char, result);
+    }
+  }
+
+  autoComplete(prefix) {
+    let node = this.findOne(prefix);
+    if (!node) return [];
+
+    let result = [];
+    this.collectWords(node, prefix, result);
+    return result;
+  }
+}
+
+const trie = new Trie();
+trie.insert("apple");
+trie.insert("appy")
+trie.insert("banana");
+
+console.log(trie.autoComplete("b"));
